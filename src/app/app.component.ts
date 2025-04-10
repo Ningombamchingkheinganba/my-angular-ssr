450b0610f3ecbcb68994022c79890414b224d3a3
Chingkheinganba <chingkheinganba.ningombam@udmglobal.com>
2025-04-10 15:36:02 +0530

    prerendering part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HttpCacheComponent } from './http-cache/http-cache.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [ HttpCacheComponent],
+    imports: [ HttpCacheComponent, RouterOutlet, RouterLink, RouterLinkActive],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-angular-ssr-app';
+  @ViewChild("element") element!: ElementRef;
+
+  ngOnInit(): void {
+    console.log(this.element);
+    
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay, withHttpTransferCacheOptions } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),provideHttpClient() ,provideClientHydration(withEventReplay(), withHttpTransferCacheOptions({
+  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideClientHydration(withEventReplay(), withHttpTransferCacheOptions({
     includePostRequests: true,
   }))]
 };
